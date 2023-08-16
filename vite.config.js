29c8c1468e8ddc91d1ad2a889857b88e6c953cee
yonatanlla <yonatanllanto174@gmail.com>
2023-08-16 01:13:32 -0500

    config  react-host-toast

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	build: {
-
 		rollupOptions: {
-			external: [], // Deja esta lista vacía
+			external: ["react-hot-toast"],
 		},
 	},
 });
