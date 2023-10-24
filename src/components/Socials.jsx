41b26051f444cea8380a9d407c4b0b05971dc2ec
yonatanlla <yonatanllanto174@gmail.com>
2023-10-23 22:04:21 -0500

    animation

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,14 +5,14 @@ import {AiOutlineInstagram} from "react-icons/ai"
 const Socials = () => {
   return (
     <div className='flex gap-7'>
-      <a href='https://github.com/YonatanLLa' target='_black' className=' cursor-pointer text-[1.5rem]'>
-        <VscGithubAlt className=' fill-purple-400'/>
+      <a href='https://github.com/YonatanLLa' target='_black' className=' cursor-pointer  text-[1.5rem]'>
+        <VscGithubAlt className=' fill-purple-400 hover:fill-sky-50 shadow-lg transition duration-300 transform hover:scale-105 '/>
       </a>
-      <a href='https://www.linkedin.com/in/yonatanllanto/' target='_black' className=' cursor-pointer text-[1.5rem]' >
-        <SlSocialLinkedin  className=' fill-purple-400'/>
+      <a href='https://www.linkedin.com/in/yonatanllanto/' target='_black' className=' cursor-pointer  text-[1.5rem]' >
+        <SlSocialLinkedin  className=' fill-purple-400 hover:fill-sky-50 shadow-lg transition duration-300 transform hover:scale-105 '/>
       </a>
-      <a href='https://www.instagram.com/frontendyona/'  target='_black'className=' cursor-pointer text-[1.5rem]'>
-        <AiOutlineInstagram  className=' fill-purple-400'/>
+      <a href='https://www.instagram.com/frontendyona/'  target='_black'className=' cursor-pointer  text-[1.5rem]'>
+        <AiOutlineInstagram  className=' fill-purple-400 hover:fill-sky-50 shadow-lg transition duration-300 transform hover:scale-105 '/>
       </a>
     </div>
   )
